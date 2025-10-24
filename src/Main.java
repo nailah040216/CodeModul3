20c74f30077dadf388707cb939f7ae9303f542e8
Nailah <nailadardjatur@gmail.com>
2025-10-24 14:27:25 +0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, ini proyek kontrol versi Java.");
-        System.out.println("Melakukan perubahan kedua."); // Baris baru
+        System.out.println("Melakukan perubahan kedua.");
+        System.out.println("ini sudah finish");// Baris baru
     }
 }
